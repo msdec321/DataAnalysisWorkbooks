688ddccfb7e730b6183ec99104544474b6c2b662
msdec321 <msdec321@gmail.com>
2021-11-26 10:23:53 -0600

    Simplified src

--- Covid19/src.py
+++ Covid19/src.py
@@ -1,16 +1,18 @@
-def total_calculator(dict_cont, loc, month, cont, variable, index):  
+def total_calculator(myDict, key, variable, loc, cont, month, year):  
+    
+    #print(key, loc, cont, month, index, variable)
     
     if loc[0] == loc[1] and month[0] != month[1]: 
         if month[0] == '12':
-            dict_cont[month[0]][0] += int(variable[0])  # Always adds December values to year 2020
+            myDict[key][cont[0]][month[0]][0] += int(variable[0])  # Always adds December values to year 2020
 
         else:
-            dict_cont[month[0]][index] += int(variable[0])  # Otherwise adds to correct year 
+            myDict[key][cont[0]][month[0]][year] += int(variable[0])  # Otherwise adds to correct year
 
     if cont[0] != cont[1]:
         if month[0] == '12':
-                dict_cont[month[0]][0] += int(variable[0])
+                myDict[key][cont[0]][month[0]][0] += int(variable[0])
 
         else:
-            dict_cont[month[0]][index] += int(variable[0])
+            myDict[key][cont[0]][month[0]][year] += int(variable[0])
 
