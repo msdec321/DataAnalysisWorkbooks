40a249b936abc8ae04e7a8445abafe75371fa7b5
msdec321 <msdec321@gmail.com>
2021-11-28 09:52:03 -0600

    Simplified total_calculator and moved if statements to main code.

--- Covid19/src.py
+++ Covid19/src.py
@@ -1,18 +1,5 @@
-def total_calculator(myDict, key, variable, loc, cont, month, year):  
+def total_calculator(myDict, key, variable, continent, month, year):  
     
-    #print(key, loc, cont, month, index, variable)
-    
-    if loc[0] == loc[1] and month[0] != month[1]: 
-        if month[0] == '12':
-            myDict[key][cont[0]][month[0]][0] += int(variable[0])  # Always adds December values to year 2020
+    myDict[key][continent][year][month] += int(variable)
 
-        else:
-            myDict[key][cont[0]][month[0]][year] += int(variable[0])  # Otherwise adds to correct year
-
-    if cont[0] != cont[1]:
-        if month[0] == '12':
-                myDict[key][cont[0]][month[0]][0] += int(variable[0])
-
-        else:
-            myDict[key][cont[0]][month[0]][year] += int(variable[0])
 
