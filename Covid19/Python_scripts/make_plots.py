ca92ae95fe05d84bffb2e9d18eff0be5a0d1e43e
msdec321 <msdec321@gmail.com>
2022-01-04 07:47:00 -0600

    Added new plots to plotting script.

--- Covid19/Python_scripts/make_plots.py
+++ Covid19/Python_scripts/make_plots.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-rollingCases = pd.read_csv(r"C:\Users\Matth\git\DataAnalysisWorkbooks\Covid19\Data\Sliced_data\rollingCases.csv")
+def make_plot(df, xCol, yCol, plotTitle, img_name):
+    _ = df.plot(x = xCol, y = yCol, style = '.', figsize = (15,5), title = plotTitle)
+    fig = _.get_figure()
+    fig.savefig("C:/Users/Matth/git/DataAnalysisWorkbooks/Covid19/Figures/" + img_name + ".png")
 
-df1 = rollingCases.set_index('location')
-rollingCasesAfrica = df1.loc['Africa']
+rollingCasesDeaths = pd.read_csv(r"C:\Users\Matth\git\DataAnalysisWorkbooks\Covid19\Data\Sliced_data\rollingCasesDeaths.csv")
 
-_ = rollingCasesAfrica.plot(x='date',y='new_cases', style='.', figsize=(15,5), title='')
-fig = _.get_figure()
-fig.savefig(r"C:\Users\Matth\git\DataAnalysisWorkbooks\Covid19\Figures\newCases_Africa.png")
+df1 = rollingCasesDeaths.set_index('location')
+continents = ['Africa', 'Asia', 'Europe', 'NorthAmerica', 'SouthAmerica', 'Oceania']
+cont_caption = ['Africa', 'Asia', 'Europe', 'North America', 'South America', 'Oceania']
+
+for i, continent in enumerate(continents):
+    make_plot(df1.loc[continent], 'date', 'new_cases', f'Daily Covid-19 Cases ({cont_caption[i]})', f'newCases_{continent}')
+    make_plot(df1.loc[continent], 'date', 'new_deaths', f'Daily Covid-19 Deaths ({cont_caption[i]})', f'newDeaths_{continent}')
+
+make_plot(df1.loc['Africa'], 'date', 'rollingCases', 'rolling cases (Africa)', 'rollingCases_Africa')
+make_plot(df1.loc['Africa'], 'date', 'rollingDeaths', 'rolling deaths (Africa)', 'rollingDeaths_Africa')
