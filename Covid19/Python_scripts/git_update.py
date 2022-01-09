f3dc7bd9b80bd2755fd9c64b68b81b6196e485a4
msdec321 <msdec321@gmail.com>
2022-01-09 09:11:47 -0600

    Adds updated scatter plot folder

--- Covid19/Python_scripts/git_update.py
+++ Covid19/Python_scripts/git_update.py
@@ -1,4 +1,4 @@
 import os
 
-os.system(""" bash -c "cd '/mnt/c/Users/Matth/git/DataAnalysisWorkbooks/Covid19' ; git add Figures ; git commit -m 'Updated figures' ; git push" """)
+os.system(""" bash -c "cd '/mnt/c/Users/Matth/git/DataAnalysisWorkbooks/Covid19/Figures' ; git add scatter_plots ; git commit -m 'Updated figures' ; git push" """)
 os.system(""" bash -c "cd '/mnt/c/Users/Matth/git/DataAnalysisWorkbooks/Covid19' ; git add Covid19_dashboard.pptx ; git commit -m 'Updated pptx dashboard' ; git push" """)
