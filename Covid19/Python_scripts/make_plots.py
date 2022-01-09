74bc8a3958cefe97676520b6a331a9108b569522
msdec321 <msdec321@gmail.com>
2022-01-09 09:20:03 -0600

    Moved figure saving to dedicated function and changed styling to seaborn.

--- Covid19/Python_scripts/make_plots.py
+++ Covid19/Python_scripts/make_plots.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use('seaborn')
 
 def make_plot(df, xCol, yCol, plotTitle, img_name):
     _ = df.plot(x = xCol, y = yCol, style = '.', figsize = (15,5), title = plotTitle)
-    fig = _.get_figure()
-    fig.savefig("C:/Users/Matth/git/DataAnalysisWorkbooks/Covid19/Figures/" + img_name + ".png")
+    save_plot(_, img_name)
+    
+def save_plot(ax, img_name):
+    fig = ax.get_figure()
+    fig.savefig("C:/Users/Matth/git/DataAnalysisWorkbooks/Covid19/Figures/scatter_plots/" + img_name + ".png")
     plt.close()
 
 data = pd.read_csv(r"C:\Users\Matth\git\DataAnalysisWorkbooks\Covid19\Data\Sliced_data\covid_data.csv")
@@ -37,9 +43,8 @@ for i, continent in enumerate(continents):
     else:
         df1.loc[continent].plot(x = 'date', y = 'total_cases', style = '.', figsize = (15,5), label = cont_caption[i], ax=ax) 
 
-fig = ax.get_figure()
-fig.savefig("C:/Users/Matth/git/DataAnalysisWorkbooks/Covid19/Figures/total_cases.png")
-plt.close()
+save_plot(ax, 'total_cases')
+    
     
 # Total Covid-19 deaths     
 for i, continent in enumerate(continents):
@@ -48,9 +53,7 @@ for i, continent in enumerate(continents):
     else:
         df1.loc[continent].plot(x = 'date', y = 'total_deaths', style = '.', figsize = (15,5), label = cont_caption[i], ax=ax) 
       
-fig = ax.get_figure()
-fig.savefig("C:/Users/Matth/git/DataAnalysisWorkbooks/Covid19/Figures/total_deaths.png")
-plt.close()    
+save_plot(ax, 'total_deaths')
     
     
 # Cases per capita per continent
@@ -60,9 +63,7 @@ for i, continent in enumerate(continents):
     else:
         df1.loc[continent].plot(x = 'date', y = 'caseRate', style = '.', figsize = (15,5), label = cont_caption[i], ax=ax)
            
-fig = ax.get_figure()
-fig.savefig("C:/Users/Matth/git/DataAnalysisWorkbooks/Covid19/Figures/casesPerCapitaPerContinent.png")
-plt.close()
+save_plot(ax, 'casesPerCapitaPerContinent')
 
 
 # Deaths per capita per continent
@@ -72,9 +73,7 @@ for i, continent in enumerate(continents):
     else:
         df1.loc[continent].plot(x = 'date', y = 'deathRate', style = '.', figsize = (15,5), label = cont_caption[i], ax=ax)
         
-fig = ax.get_figure()
-fig.savefig("C:/Users/Matth/git/DataAnalysisWorkbooks/Covid19/Figures/deathsPerCapitaPerContinent.png")
-plt.close()
+save_plot(ax, 'deathsPerCapitaPerContinent')
 
 
 # Death-case ratio per continent
@@ -85,6 +84,4 @@ for i, continent in enumerate(continents):
         df1.loc[continent].plot(x = 'date', y = 'deathCaseRatio', style = '.', figsize = (15,5), label = cont_caption[i], ax=ax) 
         
 ax.set_ylim(0, 0.11)
-fig = ax.get_figure()
-fig.savefig("C:/Users/Matth/git/DataAnalysisWorkbooks/Covid19/Figures/deathsCaseRatio.png")
-plt.close()
+save_plot(ax, 'deathsCaseRatio')
