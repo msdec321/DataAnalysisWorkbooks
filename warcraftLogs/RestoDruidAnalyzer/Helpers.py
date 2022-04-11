f55102edc80afb2853b26be773b2af81b67a7540
msdec321 <msdec321@gmail.com>
2022-04-11 09:07:37 -0500

    Added plot saving feature

--- warcraftLogs/RestoDruidAnalyzer/Helpers.py
+++ warcraftLogs/RestoDruidAnalyzer/Helpers.py
@@ -6,32 +6,47 @@ import numpy as np
 
 
 def apply_filters(df, rotating_on_tank, spriest, innervate, bloodlust, natures_grace, power_infusion, nHealers):
+    n_filters = 0
+    filters = ""
     
     if rotating_on_tank in ['Yes', 'No']:
         df = df.loc[df['Rotating on tank?'] == rotating_on_tank]
+        n_filters += 1
+        filters += f"_tankrotating{rotating_on_tank}"
 
     if nHealers in [4, 5, 6]:
         df = df.loc[df['nHealers'] == nHealers]
+        filters += f"_{nHealers}healers"
 
     if spriest in ['Yes', 'No']:
         df = df.loc[df['Spriest?'] == spriest]
+        n_filters += 1
+        filters += f"_spriest{spriest}"
 
     if innervate in ['Yes', 'No']:
         df = df.loc[df['Innervate?'] == innervate]
+        n_filters += 1
+        filters += f"_innervate{innervate}"
         
     if bloodlust in ['Yes', 'No']:
         df = df.loc[df['Bloodlust?'] == bloodlust]
+        n_filters += 1
+        filters += f"_bloodlust{bloodlust}"
         
     if natures_grace in ['Yes', 'No']:
         df = df.loc[df["Nature's Grace?"] == natures_grace]
+        n_filters += 1
+        filters += f"_naturesgrace{natures_grace}"
         
     if power_infusion in ['Yes', 'No']:
         df = df.loc[df["Power Infusion?"] == power_infusion]
+        n_filters += 1
+        filters += f"_powerinfusion{_powerinfusion}"
         
-    return df
+    return df, n_filters, filters
 
 
-def plot_rotations(boss, df):
+def plot_rotations(boss, df, n_filters, filters, save):
     rotations =  df['Rotation 1'].unique().tolist()
     rot_data = []
     
@@ -49,6 +64,7 @@ def plot_rotations(boss, df):
 
     for i in range(len(rot_data)):
         if rot_data[i][3] <= 3: continue  # Skip rotations that don't have enough data.
+        if i == 20: break # Limit the number of rotations plotted to the top 20
 
         temp_rot.append(rot_data[i][0])
         temp_mean.append(rot_data[i][1])
@@ -68,6 +84,18 @@ def plot_rotations(boss, df):
     plt.title(f'{boss}', fontsize = 20, loc = 'right', pad = 12)
     plt.xlabel('Avg HPS', fontsize = 16, loc = 'right', labelpad = 12) 
     plt.ylabel('Rotation', fontsize = 24, loc = 'top')
+    
+    if save:
+        boss = boss.replace(" ", "")
+        boss = boss.replace("'", "")
+        
+        if n_filters == 0:
+            plt.savefig(f'plots/{boss.lower()}_N{df.shape[0]}_nofilters.png', dpi = 100)
+            
+        else:
+            plt.savefig(f'plots/{boss.lower()}_N{df.shape[0]}{filters}.png', dpi = 100)
+            
+    if df.shape[0] < 100: print(f"WARNING! Low statistics! N = {df.shape[0]}. Consider using a looser set of filters.")
 
     plt.show()
     
